0b67a858c532eb04a6462c2bd106ee28c4f25d30
Lex Childs <lexchilds@gmail.com>
2011-01-11 07:45:56 -0600

    updated to get local subnet

--- lib/knoc-knoc.rb
+++ lib/knoc-knoc.rb
@@ -1,11 +1,23 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'net/ping'
+require 'socket'
 
 module KnocKnoc
   module_function
 
-  SUBNET = '10.10.10.'
+
+  def local_ip
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+    UDPSocket.open do |s|
+      s.connect '172.0.0.1', 1
+      s.addr.last
+    end
+  ensure
+    Socket.do_not_reverse_lookup = orig
+  end
+
+  SUBNET = /([0-9]+\.){3}/.match(local_ip).to_s
 
   def list
     (1..254).map do |n|
