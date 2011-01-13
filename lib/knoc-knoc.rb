ce6f18ef5828fc27493374672a6398c52ff16f32
Lex Childs <lexchilds@gmail.com>
2011-01-12 21:25:58 -0600

    refactor lib layout, start of host class

--- lib/knoc-knoc.rb
+++ lib/knoc-knoc.rb
@@ -3,12 +3,32 @@ require 'bundler/setup'
 #require 'net/ping'
 require 'ping'
 require 'socket'
-#require 'macaddr'
+
+require File.dirname(__FILE__) + '/knoc-knoc/host'
 
 module KnocKnoc
   module_function
 
 
+  @whos_there, @whos_new, @whos_left = [], [], []
+
+  def whos_there
+    @internal_list
+  end
+
+  def whos_new
+  end
+
+  def whos_left
+  end
+
+
+  def refresh_lists
+    current_ips = ping_all
+    
+  end
+
+  
   def local_ip
     orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
     UDPSocket.open do |s|
@@ -19,17 +39,10 @@ module KnocKnoc
     Socket.do_not_reverse_lookup = orig
   end
 
-  def get_hostname ip
-    Socket::getaddrinfo(ip,nil)[0][2]
-  end
 
   SUBNET = /([0-9]+\.){3}/.match(local_ip).to_s
 
-  @internal_list = []
-
-  def refresh_list
-    @internal_list = full_list 
-  end
+  private
 
   def ping_all
     threads, list = [], []
@@ -45,29 +58,7 @@ module KnocKnoc
     list
   end
 
-  def full_list
-    addresses, threads, list = ping_all, [], []
-    addresses.each do |address|
-      threads << Thread.new(address) do |a|
-          list.push( { :ip => a, :mac => get_mac(a) } )
-      end
-    end
-    threads.each { |th| th.join }
-    list
-  end
 
-  def list
-    refresh_list if @internal_list == []
-    @internal_list
-  end
 
-  def get_mac ip
-    mac = /([a-zA-Z0-9]+:)+([a-zA-Z0-9]+)/.match(`arp #{ip}`).to_s
-    mac = mac.split /:/
-    mac = mac.map{ |p| '%02s' % p }
-    mac = mac.map{ |p| p.gsub(' ', '0' ) }
-    mac = mac.join(':')
-    mac.upcase
-  end
 
 end
---
+++ lib/knoc-knoc/host.rb
@@ -0,0 +1,32 @@
+module KnocKnoc
+
+  class Host
+
+    #@ip, @hostname, @mac = nil, nil, nil
+    attr_reader :ip, :hostname, :mac
+
+    def initialize(address)
+      @ip = address
+      @hostname = get_hostname(address)
+      @mac = get_mac(address)
+    end
+
+
+    private 
+
+    def get_hostname ip
+      Socket::getaddrinfo(ip,nil)[0][2]
+    end
+
+    def get_mac ip
+      mac = /([a-zA-Z0-9]+:)+([a-zA-Z0-9]+)/.match(`arp #{ip}`).to_s
+      mac = mac.split /:/
+      mac = mac.map{ |p| '%02s' % p }
+      mac = mac.map{ |p| p.gsub(' ', '0' ) }
+      mac = mac.join(':')
+      mac.upcase
+    end
+
+  end
+
+end
